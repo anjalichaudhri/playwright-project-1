3a63d49de055129e8b60d25139b66644d03d8c2e
anjaligrapecity_1 <anjali.chander@grapecity.com>
2024-02-09 17:07:34 +0530

    first commit

---
+++ tests/example.spec.ts
@@ -0,0 +1,83 @@
+import {expect, test} from '@playwright/test';
+
+test.skip("Selectors", async({page}) => {
+    // text
+    await page.click('text=some text');
+
+    //css selectors
+    await page.click('button');
+    await page.click('#id');
+    await page.click('.class');
+
+    // only visible css selector
+    await page.click('submit-button:visible');
+
+    //combinations
+    await page.click('#username .first');
+
+    //xapth
+    await page.click('//button');
+})
+
+test('working with inputs', async({page})=> {
+    await page.goto("http://zero.webappsecurity.com/");
+    await page.click('#signin_button');
+
+    await page.fill('#user_login', 'some username');
+    await page.fill('#user_password', 'some password');
+
+    await page.click('text=Sign in');
+
+    const errorMessage = await page.locator('.alert-error');
+    await expect(errorMessage).toContainText('Login and/or password are wrong');
+})
+
+test('Assertions', async({page})=> {
+    const url = "https://www.example.com";
+    const title = "Example Domain";
+    await page.goto("https://www.example.com");
+    await expect(page).toHaveURL(url);
+    await expect(page).toHaveTitle(title);
+
+    const element = await page.locator('h1');
+
+    await expect(element).toBeVisible();
+    await expect(element).toHaveText(title);
+    await expect(element).toHaveCount(1);
+
+    const elementNotExist = await page.locator('h5');
+    await expect(elementNotExist).not.toBeVisible();
+
+    await expect(element).toBeVisible();
+});
+
+test.skip('annotations', async({page}) => {
+    //skip
+    //only
+    //describe
+});
+
+test.describe('Learn Palywright suite', () => {
+    test('Navigate to a page', async({page}) => {
+        await page.goto("https://www.example.com");
+        const pageTitle = await page.locator('h1');
+        await expect(pageTitle).toContainText('Example Domain');
+    })
+    
+    test('Click Button', async({page}) => {
+        await page.goto("http://zero.webappsecurity.com/");
+        await page.click('a.brand');
+        await page.click('#signin_button');
+        await page.click('text=Sign in');
+    
+        const errorMessage = await page.locator('.alert-error');
+        await expect(errorMessage).toContainText('Login and/or password are wrong');
+    });
+});
+
+test('tagging @my-tag @testTag', async ({page}) => {
+    await page.goto("http://zero.webappsecurity.com/");
+
+    // grep or grep-invert to run tagged tests
+});
+
